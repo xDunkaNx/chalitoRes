e5d2978df69530d4c53ace19b85461c30b2c8338
Harold Guerrero <harold.guerrero3@gmail.com>
2022-09-18 20:52:48 -0500

    Register Person desde Usuario

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,6 +20,7 @@ class User extends Authenticatable
      */
     // protected $guard_name = 'api';
     protected $fillable = [
+        'id',
         'userName',
         // 'email',
         'password',
